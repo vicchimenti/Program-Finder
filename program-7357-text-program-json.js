f0754e38b73d4237ffd35ddc49424bbb38ad7199
Vic Chimenti <victor.chimenti@gmail.com>
2025-10-15 09:24:20 -0700

    add program description

--- program-7357-text-program-json.js
+++ program-7357-text-program-json.js
@@ -18,6 +18,7 @@ try {
     list['programName'] = processTags('<t4 type="content" name="Program Title" output="normal" display_field="value" delimiter="|" />');
     list['programID'] = processTags('<t4 type="meta" meta="content_id" />');
     list['school'] = processTags('<t4 type="content" name="School" output="normal" display_field="name" delimiter="|" />');
+    list["programDepartment"] = processTags('<t4 type="content" name="Program Department" output="normal" display_field="value" delimiter="|" />');
     list['programType'] = processTags('<t4 type="content" name="Program Type" output="normal" display_field="name" delimiter="|" />');
     list['areaOfStudy'] = processTags('<t4 type="content" name="Area of Study" output="normal" display_field="value" delimiter="|" />');
     list['programDescription'] = processTags('<t4 type="content" name="Program Description" output="normal" modifiers="striptags,htmlentities" />');
