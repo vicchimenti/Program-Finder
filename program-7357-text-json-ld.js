23842cf8f5c2501d49aabbb39213af32809a738a
Vic Chimenti <victor.chimenti@gmail.com>
2025-10-15 09:23:04 -0700

    import existing

---
+++ program-7357-text-json-ld.js
@@ -0,0 +1,92 @@
+try {
+
+    function processTags(t4Tag) {
+        myContent = content || null;
+        return String(
+            com.terminalfour.publish.utils.BrokerUtils.processT4Tags(
+                dbStatement,
+                publishCache,
+                section,
+                myContent,
+                language,
+                isPreview,
+                t4Tag
+            )
+        );
+    }
+
+    var list = {};
+    list["id"] = processTags('<t4 type="meta" meta="content_id" />');
+    list["programName"] = processTags('<t4 type="content" name="Program Title" output="normal" display_field="value" delimiter="|" />');
+    list["programID"] = processTags('<t4 type="meta" meta="content_id" />');
+    list["school"] = processTags('<t4 type="content" name="School" output="normal" display_field="name" delimiter="|" />');
+    list["programDepartment"] = processTags('<t4 type="content" name="Program Department" output="normal" display_field="value" delimiter="|" />');
+    list["programType"] = processTags('<t4 type="content" name="Program Type" output="normal" display_field="name" delimiter="|" />');
+    list["areaOfStudy"] = processTags('<t4 type="content" name="Area of Study" output="normal" display_field="value" delimiter="|" />');
+    list["programDescription"] = processTags('<t4 type="content" name="Program Description" output="normal" modifiers="striptags,htmlentities" />');
+    list["learningFormat"] = processTags('<t4 type="content" name="Learning Format" output="normal" display_field="name" delimiter="|" />');
+    list["programLevel"] = processTags('<t4 type="content" name="Program Level" output="normal" display_field="name" delimiter="|" />');
+    list["url"] = processTags('<t4 type="navigation" name="Return Current Section Path" id="976" />');
+    list["programSummary"] = processTags('<t4 type="content" name="Program Summary" output="normal" modifiers="striptags,htmlentities" />');
+    list["degree"] = processTags('<t4 type="content" name="Degree" output="normal" modifiers="striptags,htmlentities" />');
+    list["duration"] = processTags('<t4 type="content" name="Duration" output="normal" modifiers="striptags,htmlentities" />');
+    list["credits"] = processTags('<t4 type="content" name="Credits" output="normal" modifiers="striptags,htmlentities" />');
+    list["keywordTags"] = processTags('<t4 type="content" name="Hidden Seach Terms" output="normal" modifiers="striptags,htmlentities" delimiter="," />');
+
+    var collegeUrls = {
+        "Albers School of Business & Economics": "https://www.seattleu.edu/business/",
+        "College of Arts & Sciences": "https://www.seattleu.edu/arts-sciences/",
+        "College of Education": "https://www.seattleu.edu/education/",
+        "College of Nursing & Health Sciences": "https://www.seattleu.edu/nursing-health-sciences/",
+        "College of Science & Engineering": "https://www.seattleu.edu/science-engineering/",
+        "Cornish College of the Arts": "https://www.cornish.edu/",
+        "School of Law": "https://law.seattleu.edu/"
+    };
+
+    var collegeName = list["school"];
+    var collegeUrl = collegeUrls[collegeName] || "https://www.seattleu.edu/";
+
+    var provider = {
+        "@type": "CollegeOrUniversity",
+        "name": "Seattle University",
+        "url": "https://www.seattleu.edu/",
+        "logo": "https://www.seattleu.edu/media/seattle-university/site-assets/branding/seattleu-logo-300x300.png"
+    };
+
+    if (collegeName) {
+        provider["school"] = {
+            "@type": "EducationalOrganization",
+            "name": collegeName,
+            "url": collegeUrl
+        };
+
+        if (list["programDepartment"]) {
+            provider["programDepartment"] = {
+                "@type": "EducationalOrganization",
+                "name": list["programDepartment"]
+            };
+        }
+    }
+
+    var jsonLD = {
+        "@context": "https://schema.org",
+        "@type": "EducationalOccupationalProgram",
+        "name": list["programName"],
+        "description": list["programSummary"] || list["programDescription"],
+        "educationalCredentialAwarded": list["degree"],
+        "timeToComplete": list["duration"],
+        "numberOfCredits": list["credits"],
+        "programMode": list["learningFormat"],
+        "programType": list["programType"],
+        "provider": provider
+    };
+
+    document.write(
+        '<script type="application/ld+json">' +
+        JSON.stringify(jsonLD) +
+        "</" + "script>"
+    );
+
+} catch (err) {
+    document.write(err);
+}
--- program-7357-text-program-json.js
+++ program-7357-text-program-json.js
@@ -1,38 +1,56 @@
 try {
-  //Defining main functions
-  function processTags(t4Tag) {
-    myContent = content || null;
-    return String(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, myContent, language, isPreview, t4Tag));
+    //Defining main functions
+    function processTags(t4Tag) {
+      myContent = content || null;
+      return String(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, myContent, language, isPreview, t4Tag));
+    }
+  
+    function getLayout(contentLayout) {
+      var tid = content.getContentTypeID();
+      formatter = contentLayout;
+      format = publishCache.getTemplateFormatting(dbStatement, tid, formatter);
+      formatString = format.getFormatting();
+      return processTags(formatString);
+    }
+  
+    var list = {};
+    list['id'] = processTags('<t4 type="meta" meta="content_id" />');
+    list['programName'] = processTags('<t4 type="content" name="Program Title" output="normal" display_field="value" delimiter="|" />');
+    list['programID'] = processTags('<t4 type="meta" meta="content_id" />');
+    list['school'] = processTags('<t4 type="content" name="School" output="normal" display_field="name" delimiter="|" />');
+    list['programType'] = processTags('<t4 type="content" name="Program Type" output="normal" display_field="name" delimiter="|" />');
+    list['areaOfStudy'] = processTags('<t4 type="content" name="Area of Study" output="normal" display_field="value" delimiter="|" />');
+    list['programDescription'] = processTags('<t4 type="content" name="Program Description" output="normal" modifiers="striptags,htmlentities" />');
+    list['programLevel'] = processTags('<t4 type="content" name="Program Level" output="normal" display_field="name" delimiter="|" />');
+    list['url'] = processTags('<t4 type="navigation" name="Return Current Section Path" id="976" />');
+    list['programSummary'] = processTags('<t4 type="content" name="Program Summary" output="normal" modifiers="striptags,htmlentities" />');
+    list['degree'] = processTags('<t4 type="content" name="Degree" output="normal" modifiers="striptags,htmlentities" />');
+    list['duration'] = processTags('<t4 type="content" name="Duration" output="normal" modifiers="striptags,htmlentities" />');
+    list['credits'] = processTags('<t4 type="content" name="Credits" output="normal" modifiers="striptags,htmlentities" />');
+    list['keywordTags'] = processTags('<t4 type="content" name="Hidden Seach Terms" output="normal" modifiers="striptags,htmlentities" delimiter="," />');
+  
+  	//manipulate learning format - only include formats in JSON which do not have a link set
+  	var learningFormats = processTags('<t4 type="content" name="Learning Format" output="normal" display_field="name" delimiter="|" />').split('|');
+  
+  	var links = {
+      "Hybrid": processTags('<t4 type="content" name="Hybrid Program Link" output="linkurl" modifiers="nav_sections" />'), 
+      "In-Person": processTags('<t4 type="content" name="In-Person Program Link" output="linkurl" modifiers="nav_sections" />'),
+      "Online": processTags('<t4 type="content" name="Online Program Link" output="linkurl" modifiers="nav_sections" />')
+    };
+  
+  	// Filter out learning formats that have a link
+    var filteredFormats = [];
+    for (var i = 0; i < learningFormats.length; i++) {
+      var format = learningFormats[i];
+      if (!(links.hasOwnProperty(format) && links[format])) {
+        filteredFormats.push(format);
+      }
+    }
+  
+  	list['learningFormat'] = filteredFormats.join('|');
+  
+    var jsonObj = new org.json.JSONObject(list);
+    document.write(jsonObj.toString() + ',');
+  } catch (err) {
+    document.write(err);
   }
-
-  function getLayout(contentLayout) {
-    var tid = content.getContentTypeID();
-    formatter = contentLayout;
-    format = publishCache.getTemplateFormatting(dbStatement, tid, formatter);
-    formatString = format.getFormatting();
-    return processTags(formatString);
-  }
-
-  var list = {};
-  list['id'] = processTags('<t4 type="meta" meta="content_id" />');
-  list['programName'] = processTags('<t4 type="content" name="Program Title" output="normal" display_field="value" delimiter="|" />');
-  list['programID'] = processTags('<t4 type="meta" meta="content_id" />');
-  list['school'] = processTags('<t4 type="content" name="School" output="normal" display_field="name" delimiter="|" />');
-  list['secondarySchool'] = processTags('<t4 type="content" name="Secondary School" output="normal" display_field="name" delimiter="|" />');
-  list['programType'] = processTags('<t4 type="content" name="Program Type" output="normal" display_field="name" delimiter="|" />');
-  list['areaOfStudy'] = processTags('<t4 type="content" name="Area of Study" output="normal" display_field="value" delimiter="|" />');
-  list['programDescription'] = processTags('<t4 type="content" name="Program Description" output="normal" modifiers="striptags,htmlentities" />');
-  list['learningFormat'] = processTags('<t4 type="content" name="Learning Format" output="normal" display_field="name" delimiter="|" />');
-  list['programLevel'] = processTags('<t4 type="content" name="Program Level" output="normal" display_field="name" delimiter="|" />');
-  list['url'] = processTags('<t4 type="navigation" name="Return Current Section Path" id="976" />');
-  list['programSummary'] = processTags('<t4 type="content" name="Program Summary" output="normal" modifiers="striptags,htmlentities" />');
-  list['degree'] = processTags('<t4 type="content" name="Degree" output="normal" modifiers="striptags,htmlentities" />');
-  list['duration'] = processTags('<t4 type="content" name="Duration" output="normal" modifiers="striptags,htmlentities" />');
-  list['credits'] = processTags('<t4 type="content" name="Credits" output="normal" modifiers="striptags,htmlentities" />');
-  list['keywordTags'] = processTags('<t4 type="content" name="Hidden Seach Terms" output="normal" modifiers="striptags,htmlentities" delimiter="," />');
-
-  var jsonObj = new org.json.JSONObject(list);
-  document.write(jsonObj.toString() + ',');
-} catch (err) {
-  document.write(err);
-}
